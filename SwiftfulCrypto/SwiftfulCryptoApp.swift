1a1a75a7ae7a1d80d9a2b27fb09ceb424044ce14
Bruno Amorim <bruno.amorim@mblabs.com.br>
2023-12-11 10:46:13 -0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Bruno Amorim on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
