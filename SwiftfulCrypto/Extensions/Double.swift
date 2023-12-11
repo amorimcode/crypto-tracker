17fb43add6a16f942919bfdebf66bebefd5caf73
amorimcode <bruno.amorim032@gmail.com>
2023-12-11 16:45:38 -0300

    create coin row view

---
+++ SwiftfulCrypto/Extensions/Double.swift
@@ -0,0 +1,8 @@
+//
+//  Double.swift
+//  SwiftfulCrypto
+//
+//  Created by Bruno Amorim on 11/12/23.
+//
+
+import Foundation
---
+++ SwiftfulCrypto/Extensions/PreviewProvider.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewProvider.swift
+//  SwiftfulCrypto
+//
+//  Created by Bruno Amorim on 11/12/23.
+//
+
+import Foundation
