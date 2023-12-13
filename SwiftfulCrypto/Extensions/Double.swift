040f9d5f4a09d666dee0689b1ccfd41d38c120ca
amorimcode <bruno.amorim032@gmail.com>
2023-12-13 16:53:47 -0300

    updates

--- SwiftfulCrypto/Extensions/Double.swift
+++ SwiftfulCrypto/Extensions/Double.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+
+
+extension Double {
+    
+    
+    /// Convert a Double into Currency with 2-2 places
+    ///```
+    ///Convert 1234.56 to $1,234.56
+    ///```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }
+    
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    /// Convert a Double into Currency with 2-6 places
+    ///```
+    ///Convert 1234.56 to $1,234.56
+    ///```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        
+        return formatter
+    }
+    
+    func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        
+        return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+    
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
